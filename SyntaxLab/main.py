7ac47faf67eed0b4130c085914810ae87aaae865
GiTechLab <bizbetter@outlook.com>
2015-03-30 23:43:32 +0800

    updated

--- SyntaxLab/main.py
+++ SyntaxLab/main.py
@@ -1,19 +1,24 @@
 __author__ = 'YS1003'
 
-import sys
+# import sys
 print('Hello Python.')
 
 
 def my_func(name, greeting_times=1):
     """Docstring: This is a test for function in Python."""
-    for index in range(0, greeting_times):
-        """if isinstance(name, object):"""
-        print('Hello, ' + name)
+    try:
+        for index in range(0, greeting_times):
+            # if isinstance(name, object):
+            print('Hello, ' + name)
+    except:
+        print('Error found..')
 
 my_func('Yang Shen')
 
 my_func('MiaoMiao', 3)
 
+my_func('MiaoMiao', 'a')
+
 print(my_func.__doc__)
 
-print(sys.path)
+# "print(sys.path)"""

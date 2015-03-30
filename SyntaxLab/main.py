f14ce1a1e76b89a106d122204aace84fed8a1678
Yang SHEN <bizbetter@outlook.com>
2015-03-30 15:50:07 +0800

    Init Code.
    Since we don't have same editions of VS, we have to start with PyCharm Community.
    
    If we need to create Django app, we have to use VS2010/2013 + PTVS.

---
+++ SyntaxLab/main.py
@@ -0,0 +1,14 @@
+__author__ = 'YS1003'
+
+print('Hello Python.')
+
+
+def my_func(name, greeting_times=1):
+    for index in range(0, greeting_times):
+        """if isinstance(name, object):"""
+        print('Hello, ' + name)
+
+my_func('Yang Shen')
+
+my_func('MiaoMiao', 3)
+

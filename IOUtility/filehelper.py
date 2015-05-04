f6e2267f2e27a8010486a10f5f57d2a500bb9d7f
Yang SHEN <bizbetter@outlook.com>
2015-05-04 23:23:30 +0800

    File operations: Refer to http://www.cnblogs.com/rollenholt/archive/2012/04/23/2466179.html

--- IOUtility/filehelper.py
+++ IOUtility/filehelper.py
@@ -1,10 +1,21 @@
 __author__ = 'Yang SHEN'
 
 import platform
+import os
 
 def get_osinfo():
+    #Refer to: http://www.cnblogs.com/rollenholt/archive/2012/04/23/2466179.html
+
     print(platform.system())
     print(platform.architecture())
-
+    print(os.getcwd())#get current dir
+    print(os.listdir(os.getcwd()))#list all folders and files
+    print(os.path.isdir(os.getcwd()))
+    print(os.path.isfile(os.getcwd()))
+    print(os.path.exists(os.getcwd()))
+    print(os.path.exists(os.getcwd() + "\\filehelper.py"))
+    print(os.path.split(os.getcwd() + "\\filehelper.py"))#get container folder and file
+    print(os.path.splitext(os.getcwd() + "\\filehelper.py"))
 
 get_osinfo()
+
